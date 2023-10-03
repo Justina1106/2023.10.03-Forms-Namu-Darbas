9fc7330eaf836aa0159c4ca7cc9df9ca295c1ee9
Justina1106 <jbycenkova@gmail.com>
2023-10-03 21:14:57 +0300

    Pridėtos 6-9 dalys

--- script.js
+++ script.js
@@ -20,17 +20,21 @@ form.addEventListener('submit', function(event) {
 
   let email = formElement.querySelector('#email').value
 
+  let FEUAgr = formElement.querySelector('#FEUA-gr').value
+
   console.log(personName)
   console.log(personSurname)
   console.log(personAge)
   console.log(phone)
   console.log(email)
+  console.log(FEUAgr)
 
   let infoWrapper = document.createElement('div')
   infoWrapper.innerHTML = `<h2>${personName}</h2>
                             <h2>${personSurname}</h2>
                             <h2>${phone}</h2>
-                           <p>${personName} ${personSurname} is ${personAge} years old and phone is ${phone} an email is ${email}....</p>`
+                            <h2>${FEUAgr}</h2>
+                           <p>${personName} ${personSurname} is ${personAge} years old and phone is ${phone} an email is ${email}. ${personName} is in ${FEUAgr}</p>`
 
   let contentElement = document.querySelector('#content')
   contentElement.append(infoWrapper)
